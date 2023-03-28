eaecf43a6a8122df938b929b92d949d2484a3c74
llDimAssll <101548861+llDimAssll@users.noreply.github.com>
2023-03-28 11:22:19 +0300

    Добавьте файлы проекта.

---
+++ JournalApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JournalApp.ViewModels.Base;
+namespace JournalApp.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title;
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+
+            }
+        }
+    }
+}
