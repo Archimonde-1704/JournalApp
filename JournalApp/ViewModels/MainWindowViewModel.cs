9ac44c5af7dafaa408bda1d7209a6b135ca5b9bc
llDimAssll <101548861+llDimAssll@users.noreply.github.com>
2023-03-29 09:01:27 +0300

    123

--- JournalApp/ViewModels/MainWindowViewModel.cs
+++ JournalApp/ViewModels/MainWindowViewModel.cs
@@ -6,14 +6,23 @@ namespace JournalApp.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+
         private string _Title;
+        /// <summary>Заголовок окна</summary>
         public string Title
         {
             get => _Title;
-            set
-            {
+            //set
+            //{
+                //if (Equals(_Title, value)) return;
+                //_Title = value;
+                //OnPropertyChanged();
 
-            }
+                //Set(ref _Title, value);
+            //}
+            set => Set (ref _Title, value);
         }
+        #endregion
     }
 }
