eb0975abc6be91e73d436fee38aee5da8dc6bec7
gozhe <gozhe@DESKTOP-21E2JCI>
2023-04-04 13:39:07 +0300

    2.02.00

--- JournalApp/Infrastructure/Commands/Base/Command.cs
+++ JournalApp/Infrastructure/Commands/Base/Command.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace JournalApp.Infrastructure.Commands.Base
 {
-    internal class Command
+    internal class Command : ICommand
     {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove +> CommandManager.RequerySuggested -= value;
+        }
+
+        private void CommandManager_RequerySuggested(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanExecute(object parameter) { throw new NotImplementedException(); }
+        public bool Execute (object parameter) { throw new NotImplementedException(); }
     }
 }
